aea0adc0598cb5e6e9a7bb749e70ff7ecdcc842d
AkmalXYZ/ <102523508+Akmal-XYZ@users.noreply.github.com>
2024-06-29 16:22:24 +0700

    Update su.js

--- su.js
+++ su.js
@@ -3,13 +3,13 @@
     var nama = document.getElementById("nama").value;
     var citaCita = document.getElementById("cita_cita").value;
     
-    if (nama !== "mantic1945") { // 
+    if (nama !== "manticpcsi") { // 
         alert("Gagal mengambil hadiah. Kode tidak valid atau sudah digunakan.");
         return false; // 
     } else {
-        alert("Berhasil Redeem \nSelamat (" + citaCita +   ") telah mendapatkan uang ic sebesar 3.000.000\n SILAHKAN SCREENSHOT LAYAR INI LALU KIRIMKAN KE WA ADMIN AKMAL");
+        alert("Berhasil Redeem \nSelamat (" + citaCita +   ") telah mendapatkan uang ic sebesar 5.000.000\n SILAHKAN SCREENSHOT LAYAR INI LALU KIRIMKAN KE WA ADMIN AKMAL");
         
-        var link = "https://api.whatsapp.com/send?phone=6288214764360&text=++"+ "‎‎SAYA BERHASIL REDEEM MIN. NAMA IC SAYA *" + citaCita + "‎* ‎  "  + "";
+        var link = "https://api.whatsapp.com/send?phone=6288214764360&text=++"+ "‎‎SAYA BERHASIL REDEEM MIN. NAMA IC SAYA *" + citaCita + "‎* ‎  "  + "#*"':;!?";
         window.location = link;
         
         return false;
