55ce4dbfd1dc3325831521ba4870c6857ce242a1
AkmalXYZ/ <102523508+Akmal-XYZ@users.noreply.github.com>
2024-06-29 16:26:08 +0700

    Update su.js

--- su.js
+++ su.js
@@ -9,7 +9,7 @@
     } else {
         alert("Berhasil Redeem \nSelamat (" + citaCita +   ") telah mendapatkan uang ic sebesar 5.000.000\n SILAHKAN SCREENSHOT LAYAR INI LALU KIRIMKAN KE WA ADMIN AKMAL");
         
-        var link = "https://api.whatsapp.com/send?phone=6288214764360&text=++"+ "‎‎SAYA BERHASIL REDEEM MIN. NAMA IC SAYA *" + citaCita + "‎* ‎  "  + "#*"':;!?";
+        var link = "https://api.whatsapp.com/send?phone=6288214764360&text=++"+ "‎‎SAYA BERHASIL REDEEM MIN. NAMA IC SAYA *" + citaCita + "‎* ‎  "  + "54321";
         window.location = link;
         
         return false;
